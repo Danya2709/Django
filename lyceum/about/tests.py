23f08718ff1e5a98bcf07cdd213266b246ac6911
Danya2709 <164789276+Danya2709@users.noreply.github.com>
2024-03-27 20:12:26 +0300

    Delete lyceum/about/tests.py

--- lyceum/about/tests.py
+++
@@ -1,9 +0,0 @@
-from http import HTTPStatus
-
-from django.test import Client, TestCase
-
-
-class StaticURLTests(TestCase):
-    def test_about_endpoint(self):
-        response = Client().get("/")
-        self.assertEqual(response.status_code, HTTPStatus.OK)
